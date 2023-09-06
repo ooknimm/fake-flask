ef5ec8513d3f035fba336c333f93f09ce7e47b10
ooknimm <ooknimm@gmail.com>
2023-09-07 00:37:05 +0900

    update sample.py

--- sample.py
+++ sample.py
@@ -2,15 +2,15 @@
 from app import FakeFlask
 
 
-pseudo_flask = FakeFlask()
+fake_flask = FakeFlask()
 
 
-@pseudo_flask.route("/")
+@fake_flask.route("/")
 def home():
     return "hello world"
 
-@pseudo_flask.route("/ping")
+@fake_flask.route("/ping")
 def ping():
     return "pong"
 
-pseudo_flask.run()
+fake_flask.run()
